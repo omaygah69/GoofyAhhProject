d413b0b202ed6b5938b4c924eb6721e130d18c06
omaygah69 <robertjohngarcia090604@gmail.com>
2024-04-08 23:23:24 +0800

    Modified the makefile because yes

--- Makefile
+++ Makefile
@@ -2,7 +2,12 @@ test: main
 	./main
 
 main: main.go
-	go build main.go
+	# go build controller.go
+	go build main.go controller.go
 
 clean:
 	rm -rf main
+	rm -rf controller
+
+run: 
+	go run main.go controller.go
--- main.go
+++ main.go
@@ -1,21 +1,41 @@
 package main
+
 import (
-    "net/http"
+	"encoding/json"
+	"net/http"
 )
+type Data struct{
+    Title string
+}
 
 func main(){
     router := http.NewServeMux()
     
     router.HandleFunc("/getshit", asd)
+    router.HandleFunc("/test", getTitle)
     
     server := http.Server{
         Addr: ":6969",
         Handler: router,
     }
-
     server.ListenAndServe()
 }
 
 func asd(w http.ResponseWriter, r *http.Request){
     w.Write([]byte("bruh"))
+}
+
+func getTitle(w http.ResponseWriter, r *http.Request){
+    sampledata := Data{
+        Title: "Nigga",
+    }
+
+    jsonData, err := json.MarshalIndent(sampledata, "", " ")
+    if err != nil {
+        http.Error(w, "Error", http.StatusInternalServerError)
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+
+    w.Write(jsonData)
 }
