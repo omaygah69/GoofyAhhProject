c79421de4b0353c266ae9a0d430330a0723efa2d
omaygah69 <robertjohngarcia090604@gmail.com>
2024-04-16 10:49:12 +0800

    Garbage Manipulation

--- controller.go
+++ controller.go
@@ -4,16 +4,22 @@ import (
     "encoding/json"
 )
 
-type Data struct{
+type Todo struct{
     Title string
+    Is_Completed bool
+}
+
+type Note struct{
+    Title string 
+    Text string 
 }
 
 func asd(w http.ResponseWriter, r *http.Request){
     w.Write([]byte("bruh"))
 }
 
-func getTitle(w http.ResponseWriter, r *http.Request){
-    sampledata := Data{
+func getTodo(w http.ResponseWriter, r *http.Request){
+    sampledata := Todo{
         Title: "Nigga",
     }
 
@@ -25,5 +31,22 @@ func getTitle(w http.ResponseWriter, r *http.Request){
     w.Header().Set("Content-Type", "application/json")
 
     w.Write(jsonData)
+}
+
+func getNotes(w http.ResponseWriter, r *http.Request){
+    sampleData := Note{
+        Title: "Note_Number 1",
+    }
+    jsonData, err := json.MarshalIndent(sampleData, "", " ")
+    if err != nil{
+        http.Error(w, "Error", http.StatusInternalServerError)
+    }
+    w.Header().Set("Content-Type", "application/json")
+
+    w.Write(jsonData)
+}
+
+func createNote(w http.ResponseWriter, r *http.Request){
+    
 }
 
--- main.go
+++ main.go
@@ -3,18 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+    "log"
 )
 
 func main(){
+
+    // File Server :
+    File_Server := http.FileServer(http.Dir("./Pages/"))
+
+    //Router:
     router := http.NewServeMux()
     
-    router.HandleFunc("/getshit", asd)
-    router.HandleFunc("/test", getTitle)
+    router.Handle("/", File_Server)
+    router.HandleFunc("/bruh", asd)
+    router.HandleFunc("/test/getTodo", getTodo)
+    router.HandleFunc("/getNote", getNotes)
     
     server := http.Server{
         Addr: ":6969",
         Handler: router,
     }
-    fmt.Printf("Started, Listening To: %v", server.Addr)
-    server.ListenAndServe()
+    fmt.Printf("Started, Listening To%v", server.Addr)
+    log.Fatal(server.ListenAndServe())
 }
